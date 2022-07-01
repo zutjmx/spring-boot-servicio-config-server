303c360a9e1358323c4fcc23fe43cdc20e68d552
Jesus Zuñiga <jesuszt@hotmail.com>
2022-07-01 17:42:16 -0500

    first commit

---
+++ src/test/java/com/zutjmx/springboot/app/config/SpringBootServicioConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zutjmx.springboot.app.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootServicioConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
